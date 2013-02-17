cac394936b62d529329c53d9cc76d80ccf5933f4
Al Graham <alastair.graham@feedhenry.com>
2013-02-17 14:03:20 +0000

    adding a basic .act call and fastclick

--- client/default/lib/app.js
+++ client/default/lib/app.js
@@ -1,32 +1,48 @@
 function appInit() {
     console.log("App Has Started");
-    $fh.act({
-        act: "getInfo",
-        req: {query: '@feedhenry'}
-    }, function(res) {
-        displayTweets(res.results);
-    }, function(err, res,test) {
-        console.log("failage",err,res);
+    $('.refreshButton').removeClass('disabled');
+    getTweets();
+
+    $('.refreshButton').on('click',function(){
+        if($(this).hasClass('disabled')) {
+            return;
+        }
+        getTweets();
     });
 }
 
 function appOffline() {
-    $('.testing').empty().append("<h2>The App appears to be offline</h2>");
+    $('.content').empty().append("<h2>The App appears to be offline</h2>");
 }
 
-function displayTweets(tweetlist) {
-    console.log("Showing",tweetlist);
+function getTweets() {
+    $('.refreshButton').addClass('disabled');
+    $fh.act({
+        act: "getInfo",
+        req: {query: '@feedhenry'}
+    }, function(res) {
+        console.log(res);
+        displayList(res.result);
+        $('.refreshButton').removeClass('disabled');
+    }, function(err, res,test) {
+        console.log("failage",err,res);
+        $('.refreshButton').removeClass('disabled');
+    });
+}
+
+function displayList(listitem) {
     var source   = $("#entry-template").html();
     var template = Handlebars.compile(source);
-    $('.testing').empty().append(template({tweetlist:tweetlist}));
+    $('.content').empty().append(template({listitem:listitem})).hide().slideDown('slow');
 }
-
 
 function onConnectionChange() {
     Utils.isOnline(function(online){
         if(online) {
-            appInit();
+            $('.refreshButton').removeClass('disabled');
+            getTweets();
         } else {
+            $('.refreshButton').addClass('disabled');
             appOffline();
         }
     });
--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,42 @@
+var async = require('async');
+var xml2js = require('xml2js');
+
+
 //Sample getInfo method that queries Twitter's feed
 module.exports.getInfo = function(args, callback){
-    // default the tag to 'cats' if there's none set
-    var query = args.query ? args.query : "cats";
-    //make the call to
+    // run getTweets and getPicasaImgs in parallel
+    async.parallel([
+      getTweets,
+      getPicasaImgs
+    ], function(err,results) {
+        var array1 = [], array2 = [], response = {};
+        for (var i = 0; i < results.length; i++) {
+            result = results[i];
+            if(result.results) {
+                response.tweets = result;
+                array1 = result.results;
+                continue;
+            }
+            if(result.entry) {
+                response.picassa = result;
+                array2 = result.entry;
+                continue;
+            }
+        }
+        response.result = array1.concat(array2).sort(dateSort);
+
+        return callback(null,response);
+    });
+};
+
+
+function getPicasaImgs(callback) {
+    var args = {};
+    args.query = "feedhenry";
     $fh.web({
-      url: "http://search.twitter.com/search.json?q="+query+"&callback=?",
+      url: "https://picasaweb.google.com/data/feed/api/all?q="+args.query+"&max-results=15",
       method: "GET",
-      contentType: 'text/json',
+      contentType: 'text/xml',
       timeout: 10000
     },function(err,data){
       if(err) {
@@ -14,6 +44,49 @@ module.exports.getInfo = function(args, callback){
         return callback("there's an error!");
       }
       var body = data.body;
-      return callback(null,body);
+      //parse the xmlresponse and turn to JS object
+      var parser = new xml2js.Parser({mergeAttrs:true,explicitArray:false});
+      //when parser is finished, return
+      parser.on('end', function(result) {
+            return callback(null,result);
+      });
+      if(body === undefined) {
+        callback("error","an unknown error has occured");
+      }
+      parser.parseString(body);
     });
-};
+}
+
+function getTweets(callback) {
+    var args = {};
+    args.query = "feedhenry";
+    $fh.web({
+      url: "http://search.twitter.com/search.json?q="+args.query+"&callback=?",
+      method: "GET",
+      contentType: 'application/json',
+      timeout: 10000
+    },function(err,data){
+      if(err) {
+        //there was an error, so return that!
+        return callback("there's an error!");
+      }
+      var body = data.body;
+      return callback(null,eval("(" + body + ")"));
+    });
+}
+
+function dateSort(a, b){
+    var timeA, timeB;
+    if(a.created_at) {
+        timeA = new Date(a.created_at);
+    } else if (a['gphoto:timestamp']) {
+        timeA = new Date(a['gphoto:timestamp']);
+    }
+    if(b.created_at) {
+        timeB = new Date(b.created_at);
+    } else if (b['gphoto:timestamp']) {
+        timeB = new Date(b['gphoto:timestamp']);
+    }
+
+    return (timeA - timeB);
+}
